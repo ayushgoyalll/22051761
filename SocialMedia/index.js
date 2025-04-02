50c595415adcef1dfaa6b94422b89330071ded7c
ayushcodes5 <goyalayush3089@gmail.com>
2025-04-02 19:17:44 +0530

    added basic express

---
+++ SocialMedia/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 9876;
+
+const API_BASE_URL = 'http://20.244.56.144/evaluation-service'; 
+
